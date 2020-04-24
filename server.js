0e3478953fe4a007c60879237326ee1ae2f3bbdf
likejean <popachs@yahoo.com>
2020-04-24 06:24:43 -0700

    Added Routes and Models Folders

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 app.use(express.static('public'));
 
